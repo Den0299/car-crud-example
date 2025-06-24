58a5610cde940d306860383359a42c001a22cf1b
Dennis Ciccone <dennisciccone02@gmail.com>
2025-06-24 10:40:37 +0200

    Initial commit: project setup

---
+++ src/main/java/com/example/car_crud_example/CarCrudExampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.car_crud_example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarCrudExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarCrudExampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/car_crud_example/CarCrudExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.car_crud_example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarCrudExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
