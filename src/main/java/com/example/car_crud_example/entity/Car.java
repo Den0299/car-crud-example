fac9291da6d69072bdecf17332d18dc0a16f4e9f
Dennis Ciccone <dennisciccone02@gmail.com>
2025-06-25 12:18:34 +0200

    Aggiunta l'entità Car con costruttore e metodi getter e setter

---
+++ src/main/java/com/example/car_crud_example/entity/Car.java
@@ -0,0 +1,46 @@
+package com.example.car_crud_example.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Car {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long carId;
+
+    private String modelName;
+    private String type;
+
+    public Car(String modelName, String type) {
+        this.modelName = modelName;
+        this.type = type;
+    }
+
+    public Long getCarId() {
+        return carId;
+    }
+
+    public void setCarId(Long carId) {
+        this.carId = carId;
+    }
+
+    public String getModelName() {
+        return modelName;
+    }
+
+    public void setModelName(String modelName) {
+        this.modelName = modelName;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+}
